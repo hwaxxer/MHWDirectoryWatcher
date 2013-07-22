49c396254b5af1ba3de145e38f5e07c947e9464e
Marcus Kida <mail@marcuskida.de>
2013-07-22 13:02:13 +0200

    Add Methods to get notified of directory changes via Block-Callback

--- MHWDirectoryWatcher.h
+++ MHWDirectoryWatcher.h
@@ -11,15 +11,24 @@ static NSString * const MHWDirectoryDidFinishChangesNotification = @"MHWDirector
 @interface MHWDirectoryWatcher : NSObject
 
 // Returns an initialized MHWDirectoryWatcher and begins to watch the path, if specified
++ (MHWDirectoryWatcher *)directoryWatcherAtPath:(NSString *)watchedPath
+                               startImmediately:(BOOL)startImmediately
+                                       callback:(void(^)())cb;
+
 + (MHWDirectoryWatcher *)directoryWatcherAtPath:(NSString *)watchPath
                                startImmediately:(BOOL)startImmediately;
 
 // Equivalent to calling +directoryWatcherAtPath:startImmediately and passing
 // YES for startImmediately.
++ (MHWDirectoryWatcher *)directoryWatcherAtPath:(NSString *)watchPath
+                                       callback:(void(^)())cb;
+
 + (MHWDirectoryWatcher *)directoryWatcherAtPath:(NSString *)watchPath;
 
 // Returns YES if started watching, NO if already is watching
 - (BOOL)startWatching;
+- (BOOL)startWatchingWithCallback:(void(^)())cb;
+
 // Returns YES if stopped watching, NO if not watching
 - (BOOL)stopWatching;
 
