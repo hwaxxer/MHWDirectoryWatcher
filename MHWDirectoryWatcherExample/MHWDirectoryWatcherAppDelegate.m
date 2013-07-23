5f9011aa52e082f4e70aae581fb7fb0ccf6c3706
Martin Hwasser <martin.hwasser@gmail.com>
2013-07-23 11:47:58 +0200

    Adds gitignore and uses correct method in example.

--- MHWDirectoryWatcherExample/MHWDirectoryWatcherAppDelegate.m
+++ MHWDirectoryWatcherExample/MHWDirectoryWatcherAppDelegate.m
@@ -69,9 +69,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [self.directoryWatcher startWatchingWithCallback:^{
-        [self directoryDidChange];
-    }];
+    [self.directoryWatcher startWatching];
 
     [NSTimer scheduledTimerWithTimeInterval:3.0
                                      target:self
